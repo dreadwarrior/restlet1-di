66f7d367dbb412a8b7f7f0dc32774e297b6c6ea6
Thomas Juhnke <tommy@van-tomas.de>
2023-04-12 16:45:37 +0200

    Create new project with maven-archetype-quickstart:1.4

---
+++ src/main/java/de/dreadlabs/App.java
@@ -0,0 +1,13 @@
+package de.dreadlabs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
